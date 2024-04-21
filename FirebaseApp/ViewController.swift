d69b788a4d881b836b0c11f7a32af23cb4b78d2b
aslanlifagan <85606751+aslanlifagan@users.noreply.github.com>
2024-04-21 23:19:21 +0400

    Initial Commit

---
+++ FirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Feqan Aslanli on 21.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
